1dd30cc2474903fb04f134d57c6dcfa0a0a68db6
Liam O'Flynn <lpoflynn@protonmail.com>
2016-06-10 22:12:25 -0700

    Updated error page

--- handle404.php
+++ handle404.php
@@ -11,6 +11,8 @@
         if (array_key_exists($ushrt, $shrts)) {
             echo "<h1>Redirecting to ".$shrts[$ushrt]."</h1>";
             echo '<META HTTP-EQUIV=REFRESH CONTENT="1; '.$shrts[$ushrt].'">';
+        } else {
+            echo "<h1>Error 404: Page not found.</h1>";
         }
     ?>
 </body>
